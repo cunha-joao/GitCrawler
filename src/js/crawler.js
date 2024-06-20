1b05f128efb997415ce0deee54d222922e9cb380
cunha-joao <cunha.joao@ipvc.pt>
2024-06-20 14:41:58 +0100

    updated crawler structure

--- src/js/crawler.js
+++ src/js/crawler.js
@@ -1,13 +1,15 @@
-//Antes de executar instalar axios e cheerio com npm install axios cheerios
+//Antes de executar instalar axios e cheerio com npm install axios cheerio
 
 const axios = require('axios');
 const cheerio = require('cheerio');
 const fs = require('fs');
 
+const query = "path:**/.env db_password= db_host="
 const url = 'https://github.com/search?q=path%3A**%2F.env%20db_password%3D%20db_host%3D&type=code';
 
 const cookies = '_octo=GH1.1.338388154.1708457847; _device_id=6bb985c7f358441a51c1be06aa2a8379; user_session=IB50Cy1acYo8Haf0KGBpb5Q6FZtMJAHSeBOYpmRKks7ss0mK; logged_in=yes; dotcom_user=cunha-joao; tz=Europe%2FLisbon;';
 
+//Baixa a página dos resultados
 async function downloadPage() {
     try {
         const response = await axios.get(url, {
@@ -18,8 +20,8 @@ async function downloadPage() {
         });
         const html = response.data;
 
-        fs.writeFileSync('page.html', html, 'utf-8');
-        console.log('Página HTML baixada e salva com sucesso.');
+        fs.writeFileSync('resultspage.html', html, 'utf-8');
+        console.log('Página HTML baixada com sucesso.');
 
         const $ = cheerio.load(html);
         $('a').each((index, element) => {
@@ -31,4 +33,16 @@ async function downloadPage() {
     }
 }
 
+//Filtra os urls com ficheiros .env
+async function findFileUrl() {
+    console.log("Sleeping..")
+}
+
+//Guarda o conteúdo dos ficheiros .env
+async function getFileContent() {
+    console.log("Sleeping...")
+}
+
 downloadPage();
+findFileUrl();
+getFileContent();
